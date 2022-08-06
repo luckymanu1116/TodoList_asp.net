2a1a21f7893d30b7beb06cdbba6a6f804c11bfc1
Manoj Reddy <luckymanoj1116@gmail.com>
2022-08-05 21:17:08 -0400

    initial commit

---
+++ Models/TodoModel.cs
@@ -0,0 +1,10 @@
+namespace manoj.Models
+{
+    public class TodoModel
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+
+        public int? Duration { get; set; }
+    }
+}
